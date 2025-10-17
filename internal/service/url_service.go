9a1dd3d554b02ddd40eadd3ce2099e7bb3c09fdd
Rian Lucas <rianluccas08@gmail.com>
2025-10-16 22:15:31 -0300

    feat: implement retry for duplicated short code

--- internal/service/url_service.go
+++ internal/service/url_service.go
@@ -1,12 +1,14 @@
 package service
 
 import (
+	"errors"
 	"fmt"
 
 	"github.com/rianlucas/url-shortener/internal/database/repositories"
 	"github.com/rianlucas/url-shortener/internal/dto"
 	"github.com/rianlucas/url-shortener/internal/models"
 	"github.com/rianlucas/url-shortener/pkg/shortcode"
+	"go.mongodb.org/mongo-driver/v2/mongo"
 )
 
 type UrlService struct {
@@ -23,6 +25,22 @@ func (u *UrlService) Create(urlDto dto.CreateUrlDto) (models.Url, error) {
 	if err != nil {
 		return models.Url{}, fmt.Errorf("error generating short code: %w", err)
 	}
+
+	maxAttempts := 3
+	for i := 1; i < maxAttempts; i++ {
+		fmt.Printf("tentativa: %v\n shortCode: %s\n", i, generatedCode)
+		result, err := u.FindByShortCode(generatedCode)
+		if errors.Is(err, mongo.ErrNoDocuments) {
+			break
+		}
+		if result.ShortCode == generatedCode {
+			generatedCode, _ = shortcode.Generate(6)
+		}
+		if i == maxAttempts-1 && result.ShortCode == generatedCode {
+			return models.Url{}, fmt.Errorf("error generating short code")
+		}
+	}
+
 	urlDto.ShortCode = generatedCode
 
 	return u.repository.Create(urlDto)
